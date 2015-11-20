07b825fbe6193e7297548dda425be3dddefdb53c
Ilya Smelkov <triplepointfive@gmail.com>
2015-11-20 21:59:40 +0300

    Term show

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,48 @@
 
 using namespace std;
 
+
 namespace FJ {
     class Term {
+    public:
+        virtual ~Term() { }
+
+        virtual string Show() = 0;
     };
 
     typedef vector<Term *> Arguments;
 
+    string show(Arguments args) {
+        string acc = "";
+        bool firstElement = true;
+
+        for (auto &element : args) {
+            if (firstElement) {
+                firstElement = false;
+            } else {
+                acc += ", ";
+            }
+            acc += element->Show();
+        }
+        return acc;
+    }
+
     class Variable : public Term {
+    public:
         Variable(const string name) {
             this->name = name;
         }
 
         string getName() const { return name; }
 
+        string Show() override { return name; }
+
     private:
         string name;
     };
 
     class Access : public Term {
+    public:
         Access(Term *object, const string propertyName) {
             this->object = object;
             this->propertyName = propertyName;
@@ -28,7 +52,11 @@ namespace FJ {
 
         string getName() const { return propertyName; }
 
-        ~Access() {
+        string Show() override {
+            return "(" + object->Show() + ")." + propertyName;
+        }
+
+        virtual ~Access() {
             delete object;
         }
 
@@ -38,13 +66,18 @@ namespace FJ {
     };
 
     class Invocation : public Term {
+    public:
         Invocation(Term *object, string methodName, Arguments args) {
             this->object = object;
             this->methodName = methodName;
             this->args = args;
         }
 
-        ~Invocation() {
+        string Show() override {
+            return object->Show() + "." + methodName + "(" + show(args) + ")";
+        }
+
+        virtual ~Invocation() {
             delete object;
             for (auto &term : args) {
                 delete term;
@@ -58,18 +91,58 @@ namespace FJ {
     };
 
     class Constructor : public Term {
+    public:
         Constructor(string className, Arguments args) {
             this->className = className;
             this->args = args;
+        }
+
+        string Show() override {
+            return "new " + className + "(" + show(args) + ")";
+        }
+
+        virtual ~Constructor() {
+            for (auto &term : args) {
+                delete term;
+            }
         }
 
     private:
         string className;
         Arguments args;
     };
+
+    class Coercion : public Term {
+    public:
+        Coercion(string className, Term *object) {
+            this->className = className;
+            this->object = object;
+        }
+
+        string Show() override {
+            return "(" + className + ") " + object->Show();
+        }
+
+        virtual ~Coercion() {
+            delete object;
+        }
+
+    private:
+        string className;
+        Term *object;
+    };
 }
 
 int main() {
-    cout << "Hello, World!" << endl;
+    FJ::Arguments args;
+
+    args.push_back(new FJ::Variable("newfst"));
+
+    FJ::Variable *thisObj = new FJ::Variable("this");
+    args.push_back(new FJ::Access(thisObj, "snd"));
+
+    FJ::Constructor *c = new FJ::Constructor("Pair", args);
+    cout << c->Show();
+    delete c;
     return 0;
 }
