c58f06308d9580c4448ab03bb236f8a0951dad83
Ilya Smelkov <triplepointfive@gmail.com>
2015-12-03 19:05:06 +0300

    Context test

---
+++ test/context_test.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+
+#include "context.h"
+
+TEST (Context, classHasProperty) {
+    PropertyName fst("fst");
+    PropertyName snd("snd");
+
+    ClassName objectClassName("Object");
+    ClassName pairClassName("Pair");
+
+    Properties properties;
+    properties[fst] = objectClassName;
+    properties[snd] = objectClassName;
+    ObjectClassBody objectClass;
+
+    ClassBody pairClass(pairClassName, properties, Methods(), &objectClass);
+
+    Context ctx;
+    ctx.addClass(pairClassName, &pairClass);
+
+    EXPECT_TRUE(ctx.classHasProperty(pairClassName, fst));
+    EXPECT_TRUE(ctx.classHasProperty(pairClassName, snd));
+}
--- test/main_test.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-
-int add(int value1, int value2) {
-    return (value1 + value2);
-}
-
-TEST (AddTest, PositiveNos) {
-    EXPECT_EQ (16, add(8, 8));
-}
