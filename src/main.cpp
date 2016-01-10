7ce178a02c2126b38b3e87e3be80b9e42164e130
Ilya Smelkov <triplepointfive@gmail.com>
2016-01-10 20:07:55 +0300

    Fix submodule

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,9 @@
 #include <vector>
 
 #include "term.h"
-#include "tokenizer/tokenizer.h"
 
 using namespace std;
 
 int main() {
-    Tokenizer tokenizer("/home/ilya/Projects/fj/examples/simple_class.java");
-    std::vector<Token*> tokens = tokenizer.tokenize();
-    for (auto elem : tokens) {
-        std::cout << elem->show() << std::endl;
-    }
     return 0;
 }
