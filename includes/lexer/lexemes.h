10d7ed521d412dd68750b096cffaba9893619152
Ilya Smelkov <triplepointfive@gmail.com>
2016-01-18 08:59:19 +0300

    Reorganize lexemes

--- includes/lexer/lexemes.h
+++ includes/lexer/lexemes.h
@@ -3,49 +3,38 @@
 
 #include <pegtl.hh>
 
+#define pegtl_char_t(x) pad < one < x >, space, space >
+
 namespace fj {
     using namespace pegtl;
 
     /* Keywords */
-    struct class_keyword : pegtl_string_t( "class" ) {};
-    struct extends_keyword : pegtl_string_t( "extends" ) {};
-    struct super_keyword : pegtl_string_t( "super" ) {};
-    struct return_keyword : pegtl_string_t( "return" ) {};
-    struct new_keyword : pegtl_string_t( "new" ) {};
-    struct this_keyword : pegtl_string_t( "this" ) {};
+
+    struct class_keyword   : pegtl_string_t("class")   {};
+    struct extends_keyword : pegtl_string_t("extends") {};
+    struct super_keyword   : pegtl_string_t("super")   {};
+    struct return_keyword  : pegtl_string_t("return")  {};
+    struct new_keyword     : pegtl_string_t("new")     {};
+    struct this_keyword    : pegtl_string_t("this")    {};
 
     /* Special characters with optional paddies */
-    struct semicolon : pad < one < ';' >, space, space > {};
-    struct open_brace : pad < one < '{' >, space, space > {};
-    struct close_brace : pad < one < '}' >, space, space > {};
-    struct open_bracket : pad < one < '(' >, space, space > {};
-    struct close_bracket : pad < one < ')' >, space, space > {};
-    struct comma : pad < one < ',' >, space, space > {};
-    struct assign : pad < one < '=' >, space, space > {};
 
-    /* Grammar */
-    struct class_name : plus< identifier > {};
+    struct semicolon     : pegtl_char_t(';') {};
+    struct open_brace    : pegtl_char_t('{') {};
+    struct close_brace   : pegtl_char_t('}') {};
+    struct open_bracket  : pegtl_char_t('(') {};
+    struct close_bracket : pegtl_char_t(')') {};
+    struct comma         : pegtl_char_t(',') {};
+    struct assign        : pegtl_char_t('=') {};
+    struct dot           : pegtl_char_t('.') {};
 
+    /* Identifiers */
+
+    struct class_name  : plus< identifier > {};
     struct method_name : plus< identifier > {};
-
-    // Could be used for both property and argument names.
     struct object_name : plus< identifier > {};
 
-    // Matches the name of new class.
-    struct declared_class_name : class_name {};
-
-    // Matches the class name after the extends keyword.
-    struct inherited_class_name : class_name {};
-
-    // Matches new property inside class body.
-    struct property_def : seq < class_name, space, object_name > {};
-
-    // A list of base units inside class body: properties, methods etc.
-    struct class_terms : sor < property_def > {};
-
-    // Ignores mandatory leading and trailing spaces.
-    template < typename Rule >
-    struct lexeme : seq < space, Rule, space> {};
+    /* Meta rules */
 
     // Matches the pattern like "{Rule}"
     template < typename Rule >
@@ -55,26 +44,28 @@ namespace fj {
     template < typename Rule >
     struct sur_with_brackets : seq < open_bracket, Rule, close_bracket > {};
 
-    /* Terms */
+    /* Method arguments */
+
+    // Matches single method argument, like "Object x".
+    struct method_arg : seq < class_name, space, object_name > {};
+
+    // Matches "Object x, ..." or just nothing for method with no arguments.
+    struct method_arguments : opt < list < method_arg, comma > > {};
+
+    /* Method terms */
 
     struct method_term; // Forwarding declaration for cycle usage.
-
-    // Matches invocation of super.
-    struct super_invocation :
-            seq < super_keyword, sur_with_brackets < success > > {};
 
     // Just a variable name, matching general restrictions.
     struct variable_term : object_name {};
 
     // Left part of assignment, matches "this.fst".
-    struct property_invocation :
-            seq < this_keyword, one < '.' >, object_name> {};
+    struct property_invocation : seq < this_keyword, dot, object_name> {};
 
     // Matches a list of terms used for invocation a method.
     struct method_list_of_args : opt < list < method_term, comma > >{};
 
-    struct method_invocation_head :
-            seq < object_name, one < '.' >, method_name > {};
+    struct method_invocation_head : seq < object_name, dot, method_name > {};
 
     // Call of a method, this includes both object method called for,
     // method name and the list of arguments.
@@ -97,20 +88,17 @@ namespace fj {
     struct return_stat : seq < pad < return_keyword, space, space >,
             method_term > {};
 
-    /* Method definitions */
+    /* Constructors */
 
-    // Matches single method argument, like "Object x".
-    struct method_arg : seq < class_name, space, object_name > {};
-
-    // Matches "Object x, ..." or just nothing for method with no arguments.
-    struct method_arguments : opt < list < method_arg, comma > > {};
-
-    // Matches constructor definition, in this case it should match class name.
     struct constructor_name : class_name {};
 
     // Matches constructor name and its arguments list.
     struct constructor_head : seq < constructor_name,
             sur_with_brackets < method_arguments > > {};
+
+    // Matches invocation of super.
+    struct super_invocation :
+        seq < super_keyword, sur_with_brackets < success > > {};
 
     // Matches the whole content of constructor body.
     // TODO: Throw local error if failed to match
@@ -120,6 +108,8 @@ namespace fj {
     // Matches the whole constructor definition.
     struct constructor_def : seq < constructor_head,
             sur_with_braces < constructor_body > > {};
+
+    /* Methods */
 
     // Matches common method declaration, with return type and name.
     struct method_ret_and_name : seq < class_name, space, method_name > {};
@@ -136,12 +126,24 @@ namespace fj {
 
     /* Classes */
 
-    struct declared_class_name_padded : lexeme < declared_class_name > {};
-    struct inherited_class_name_padded : lexeme < inherited_class_name > {};
+    // Matches the name of new class.
+    struct declared_class_name : class_name {};
+
+    // Matches the class name after the extends keyword.
+    struct inherited_class_name : class_name {};
+
+    struct declared_class_name_padded : pad < declared_class_name, space > {};
+    struct inherited_class_name_padded : pad < inherited_class_name, space > {};
 
     // Matches "class A extends B"
     struct class_header : if_must < class_keyword, declared_class_name_padded,
         extends_keyword, inherited_class_name_padded > {};
+
+    // Matches new property inside class body.
+    struct property_def : seq < class_name, space, object_name > {};
+
+    // A list of base units inside class body: properties, methods etc.
+    struct class_terms : sor < property_def > {};
 
     // Matches the content of {"..."}.
     struct class_body : seq < list < class_terms, semicolon >, semicolon > {};
